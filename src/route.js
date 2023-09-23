652faac676fa46edbfd733f2e51d66cdb3c46110
chialerk <chiaerk@gmail.com>
2023-09-23 19:43:08 +0800

    - update code

--- src/route.js
+++ src/route.js
@@ -8,7 +8,7 @@ export default function DefaultRoute() {
     return (
         <BrowserRouter>
             <Routes>
-            <Route path="portfolio" element={<Home />} />
+            <Route path="" element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="contact" element={<Contact />} />
                 <Route path="*" element={<NoPage />} />
